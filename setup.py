8f6e6522598c996b2d74623b38f602d8734841fe
Lx Yu <i@lxyu.net>
2014-03-20 15:32:43 +0800

    typo bug fix. release 0.1.1.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="supervisor-quick",
-    version="0.1.0",
+    version="0.1.1",
     description="Bypass supervisor's nasty callbacks stack and make it quick!",
     author="Lx Yu",
     author_email="i@lxyu.net",
-    py_modules=["supervisor-quick", ],
+    py_modules=["supervisor_quick", ],
     package_data={"": ["LICENSE"], },
     url="http://lxyu.github.io/supervisor-quick/",
     license="MIT",
